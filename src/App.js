61d37cb9256564a4cea8dfbef9c1a577c70fc0fa
Kishara Manilka <kisharamanilka0@gmail.com>
2025-03-12 11:11:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const chatEndRef = useRef(null);
-  const backendUrl = 'http://localhost:5002';
+  const backendUrl = 'http://13.234.31.155/';
 
   useEffect(() => {
     chatEndRef.current?.scrollIntoView({ behavior: 'smooth' });
